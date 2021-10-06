e8425d82d1865fda8832a01cbb476ebd289a4c30
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-05 23:36:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_security_rule" "myrule1" {
+resource "azurerm_network_security_rule" "mynsgrule1" {
   name                        = "test123"
   priority                    = 100
   direction                   = "Outbound"
@@ -8,6 +8,6 @@ resource "azurerm_network_security_rule" "myrule1" {
   destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name         = var.rg-name
+  network_security_group_name = var.nsg-name
 }
